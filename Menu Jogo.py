c9c2d92cbd584735a5028a45e4b7f197c1cd7298
AMFnino <103612700+AMFnino@users.noreply.github.com>
2022-05-19 20:43:01 -0300

    Update Menu Jogo.py

--- Menu Jogo.py
+++ Menu Jogo.py
@@ -1,14 +1,31 @@
+"""
+---------------- GRUPO AGL --------------------
+
+André de Miche Fialho – 42076218
+
+Guilherme Henrique de Moura Guedes – 42010012
+
+Lucca Romano Mari Mancusi – 42031575
+
+-----------------------------------------------
+
+"""
 
 import pygame_menu
 from pygame_menu.examples import create_example_window
 
 from typing import Tuple, Any
+import jogo
 
-surface = create_example_window('MENU - GRUPO AGL', (600, 400))
+surface = create_example_window('MENU - GRUPO AGL', (400, 600))
+
 
 def start_the_game() -> None:
 
     global user_name
+
+    jogo.play()
+
 
 menu = pygame_menu.Menu(
     height=300,
@@ -19,7 +36,7 @@ menu = pygame_menu.Menu(
 
 user_name = menu.add.text_input('Nome: ', default='       ', maxchar=10)
 user_name = menu.add.text_input('Idade: ', default='', maxchar=2)
-menu.add.selector('País: ', [('Brasil', 1), ('Argentina', 2), ('Chile', 3), ('Uruguai', 4), ('Paraguai', 5), ('Outro', 6)]) 
+menu.add.selector('País: ', [('Brasil', 1), ('Argentina', 2), ('Chile', 3), ('Uruguai', 4), ('Paraguai', 5), ('Outro', 6)]) #, onchange=set_difficulty)
 menu.add.button('Jogar', start_the_game)
 menu.add.button('Sair', pygame_menu.events.EXIT)
 
